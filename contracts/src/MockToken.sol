75968be120e9cb520229783f8c8c52303f8dae81
JFKongphop <kongphopleo@gmail.com>
2026-04-09 01:59:15 +0700

    add: mock token for zringotts contract

---
+++ contracts/src/MockToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20 {
+  uint8 private _customDecimals;
+
+  constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 initialSupply) ERC20(_name, _symbol) {
+    _customDecimals = _decimals;
+    _mint(msg.sender, initialSupply);
+  }
+
+  function mint(address to, uint256 amount) external {
+    _mint(to, amount);
+  }
+
+  function decimals() public view override returns (uint8) {
+    return _customDecimals;
+  }
+}
